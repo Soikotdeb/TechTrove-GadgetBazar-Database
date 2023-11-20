7c4413bcb13ea15d1686e1cd36280f33db36cb6c
soikotdeb <sdsoikot424@gmail.com>
2023-11-20 19:59:03 +0600

    limit number changed

--- index.js
+++ index.js
@@ -26,7 +26,7 @@ const client = new MongoClient(uri, {
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
+    // await client.connect();
 
 // All Collection Start-----------------------------------------------------------------------------------------------------
 const usersCollection = client.db("TechTrove").collection("AllUsers");
